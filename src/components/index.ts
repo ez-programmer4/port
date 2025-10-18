58feea8cd0bd2bf37f704bd68d31cf916da29cdb
ezedin ebrahim <ezedinebrahim131@gmail.com>
2025-10-18 15:30:09 +0300

    Remove unused Technologies component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export { default as Hero } from "./Hero";
 export { default as About } from "./About";
 export { default as Experience } from "./Experience";
 export { default as Projects } from "./Projects";
-export { default as Technologies } from "./Technologies";
 export { default as Testimonials } from "./Testimonials";
 export { default as Contact } from "./Contact";
 export { default as Footer } from "./Footer";
